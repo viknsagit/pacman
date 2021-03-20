46ccc6f374b7f95cc2a916cfabcb3a7fbd39b379
Никита <SpinFirePC@outlook.com>
2021-03-20 12:16:30 +0500

    demo v0.03(Работает счетчик и поедание точек)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,38 +29,23 @@ namespace pacman
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.eb3 = new System.Windows.Forms.PictureBox();
             this.pacmanpic = new System.Windows.Forms.PictureBox();
             this.eb1 = new System.Windows.Forms.PictureBox();
-            this.eb6 = new System.Windows.Forms.PictureBox();
+            this.eb3 = new System.Windows.Forms.PictureBox();
             this.eb2 = new System.Windows.Forms.PictureBox();
-            this.eb5 = new System.Windows.Forms.PictureBox();
             this.eb4 = new System.Windows.Forms.PictureBox();
             this.lscore = new System.Windows.Forms.Label();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.eb3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pacmanpic)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.eb1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.eb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eb3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.eb2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.eb5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.eb4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // eb3
-            // 
-            this.eb3.Image = global::pacman.Properties.Resources.ball;
-            this.eb3.Location = new System.Drawing.Point(256, 279);
-            this.eb3.Name = "eb3";
-            this.eb3.Size = new System.Drawing.Size(16, 15);
-            this.eb3.TabIndex = 1;
-            this.eb3.TabStop = false;
             // 
             // pacmanpic
             // 
             this.pacmanpic.Image = global::pacman.Properties.Resources.rightp;
-            this.pacmanpic.Location = new System.Drawing.Point(37, 12);
+            this.pacmanpic.Location = new System.Drawing.Point(22, 156);
             this.pacmanpic.Name = "pacmanpic";
             this.pacmanpic.Size = new System.Drawing.Size(36, 35);
             this.pacmanpic.TabIndex = 0;
@@ -69,46 +54,37 @@ namespace pacman
             // eb1
             // 
             this.eb1.Image = global::pacman.Properties.Resources.ball;
-            this.eb1.Location = new System.Drawing.Point(290, 24);
+            this.eb1.Location = new System.Drawing.Point(207, 100);
             this.eb1.Name = "eb1";
             this.eb1.Size = new System.Drawing.Size(16, 15);
             this.eb1.TabIndex = 5;
             this.eb1.TabStop = false;
             // 
-            // eb6
+            // eb3
             // 
-            this.eb6.Image = global::pacman.Properties.Resources.ball;
-            this.eb6.Location = new System.Drawing.Point(359, 120);
-            this.eb6.Name = "eb6";
-            this.eb6.Size = new System.Drawing.Size(16, 15);
-            this.eb6.TabIndex = 6;
-            this.eb6.TabStop = false;
+            this.eb3.Image = global::pacman.Properties.Resources.ball;
+            this.eb3.Location = new System.Drawing.Point(244, 176);
+            this.eb3.Name = "eb3";
+            this.eb3.Size = new System.Drawing.Size(16, 15);
+            this.eb3.TabIndex = 6;
+            this.eb3.TabStop = false;
             // 
             // eb2
             // 
             this.eb2.Image = global::pacman.Properties.Resources.ball;
-            this.eb2.Location = new System.Drawing.Point(278, 172);
+            this.eb2.Location = new System.Drawing.Point(319, 224);
             this.eb2.Name = "eb2";
             this.eb2.Size = new System.Drawing.Size(16, 15);
             this.eb2.TabIndex = 7;
             this.eb2.TabStop = false;
             // 
-            // eb5
-            // 
-            this.eb5.Image = global::pacman.Properties.Resources.ball;
-            this.eb5.Location = new System.Drawing.Point(416, 163);
-            this.eb5.Name = "eb5";
-            this.eb5.Size = new System.Drawing.Size(16, 15);
-            this.eb5.TabIndex = 8;
-            this.eb5.TabStop = false;
-            // 
             // eb4
             // 
             this.eb4.Image = global::pacman.Properties.Resources.ball;
-            this.eb4.Location = new System.Drawing.Point(442, 307);
+            this.eb4.Location = new System.Drawing.Point(365, 100);
             this.eb4.Name = "eb4";
             this.eb4.Size = new System.Drawing.Size(16, 15);
-            this.eb4.TabIndex = 9;
+            this.eb4.TabIndex = 8;
             this.eb4.TabStop = false;
             // 
             // lscore
@@ -120,12 +96,6 @@ namespace pacman
             this.lscore.TabIndex = 10;
             this.lscore.Text = "0";
             // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 5000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -133,22 +103,18 @@ namespace pacman
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lscore);
             this.Controls.Add(this.eb4);
-            this.Controls.Add(this.eb5);
             this.Controls.Add(this.eb2);
-            this.Controls.Add(this.eb6);
-            this.Controls.Add(this.eb1);
             this.Controls.Add(this.eb3);
+            this.Controls.Add(this.eb1);
             this.Controls.Add(this.pacmanpic);
             this.Name = "Form1";
             this.Text = "PacmanDemo";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.eb3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pacmanpic)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.eb1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.eb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eb3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.eb2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.eb5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.eb4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -158,14 +124,11 @@ namespace pacman
         #endregion
 
         private System.Windows.Forms.PictureBox pacmanpic;
-        private System.Windows.Forms.PictureBox eb3;
         private System.Windows.Forms.PictureBox eb1;
-        private System.Windows.Forms.PictureBox eb6;
+        private System.Windows.Forms.PictureBox eb3;
         private System.Windows.Forms.PictureBox eb2;
-        private System.Windows.Forms.PictureBox eb5;
         private System.Windows.Forms.PictureBox eb4;
         private System.Windows.Forms.Label lscore;
-        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -40,6 +40,7 @@ namespace pacman
         {
             if (e.KeyCode == Keys.W)
             {
+                pacmanpic.Top -= 10;
                 pacmanpic.Image = Properties.Resources.сt1;
 
             }
@@ -58,28 +59,93 @@ namespace pacman
                 pacmanpic.Left -= -10;
                 pacmanpic.Image = Properties.Resources.crl1;
             }
-
-
-            int eb1l = eb1.Left;
-
             lscore.Text = Convert.ToString(score);
-        }
-
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
+            int eb1l = eb1.Left;
             int x1 = pacmanpic.Left;
             int y1 = pacmanpic.Top;
             int w1 = pacmanpic.Width;
             int h1 = pacmanpic.Height;
+
             int x2 = eb1.Left;
             int y2 = eb1.Top;
             int w2 = eb1.Width;
             int h2 = eb1.Height;
-            if ((x1 + w1 > x2) & (x1 < x2 + w2) & (y1 + h1 == y2) & (y1 < y2 + h2)) ;
+
+            int x3 = eb2.Left;
+            int y3 = eb2.Top;
+            int w3 = eb2.Width;
+            int h3 = eb2.Height;
+
+            int x4 = eb3.Left;
+            int y4 = eb3.Top;
+            int w4 = eb3.Width;
+            int h4 = eb3.Height;
+
+            int x5 = eb4.Left;
+            int y5 = eb4.Top;
+            int w5 = eb4.Width;
+            int h5 = eb4.Height;
+
+
+
+            for (int err = 0; score >= 0;)
             {
-                score++;
-                eb1.Visible = false;
+
+
+                if ((x1 + w1 >= x2) & (x2 + w2 >= x1) & (y1 + h1 >= y2) & (y2 + h2 >= y1))
+                {
+                    score++;
+                    eb1.Visible = false;
+                    eb1.Left = -100;
+
+                }
+                break;
+
+
+
+            }
+            for (int err = 0; score >= 0;)
+            {
+
+
+                if ((x1 + w1 >= x3) & (x3 + w3 >= x1) & (y1 + h1 >= y3) & (y3 + h3 >= y1))
+                {
+                    score++;
+                    eb2.Visible = false;
+                    eb2.Left = -100;
+                }
+                break;
+
+
+            }
+            for (int err = 0; score >= 0;)
+            {
+
+
+                if ((x1 + w1 >= x4) & (x4 + w4 >= x1) & (y1 + h1 >= y4) & (y4 + h4 >= y1))
+                {
+                    score++;
+                    eb3.Visible = false;
+                    eb3.Left = -100;
+                }
+                break;
+
+
+
+            }
+            for (int err = 0; score >= 0;)
+            {
+
+
+                if ((x1 + w1 >= x5) & (x5 + w5 >= x1) & (y1 + h1 >= y5) & (y5 + h5 >= y1))
+                {
+                    score++;
+                    eb4.Visible = false;
+                    eb4.Left = -100;
+
+                }
+                break;
+
 
             }
         }
